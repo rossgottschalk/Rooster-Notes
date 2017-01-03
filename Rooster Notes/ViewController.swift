b4ecc9bac431a858431a22ee6943acb6237dce11
Ross Gottschalk <rossgottschalk@yahoo.com>
2017-01-03 13:59:00 -0500

    Basic Framework. CloudKit next

--- Rooster Notes/ViewController.swift
+++ Rooster Notes/ViewController.swift
@@ -8,18 +8,70 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITextFieldDelegate
+ {
+    
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var noteTakingTextFieldBottomContraint: NSLayoutConstraint!
+    @IBOutlet weak var noteTakingTextField: UITextField!
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardDidShow), name: NSNotification.Name.UIKeyboardDidShow, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: NSNotification.Name.UIKeyboardWillHide, object: nil)
+    
+        
+        scrollView.keyboardDismissMode = .interactive
+        
+        //scrollView.keyboardDismissMode = .interactive
+        
     }
 
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    deinit
+    {
+        //if you add observers, you HAVE TO remove them
+        NotificationCenter.default.removeObserver(self)
+    }
 
+    
+    //MARK: - Helper functions
+    func keyboardDidShow(_ notification: Notification)
+    {
+        let height = (notification.userInfo![UIKeyboardFrameEndUserInfoKey] as AnyObject).cgRectValue.height
+        noteTakingTextFieldBottomContraint.constant = height + 4
+    }
+    
+    func keyboardWillHide (_ notification: Notification)
+    {
+        noteTakingTextFieldBottomContraint.constant = 8.0
+    }
+    
+    func hideKeyboard() {
+        noteTakingTextField.resignFirstResponder()
+    }
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        noteTakingTextField.resignFirstResponder()
+    }
+
+//    @IBAction func swipeDownKeyboard(_ sender: UISwipeGestureRecognizer)
+//    {
+//        if sender.direction == .down
+//        {
+//        noteTakingTextField.resignFirstResponder()
+//        }
+//            //gestureNameLabel.text = "Swipe Right"
+//        
+//    }
 
 }
-
